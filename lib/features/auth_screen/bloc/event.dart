4b85d34bd376b07b7e6f136489da0af82338f83d
encer <encer.e@yandex.ru>
2024-10-14 15:26:28 +0300

    Fixed issue with reauthorization

--- lib/features/auth_screen/bloc/event.dart
+++ lib/features/auth_screen/bloc/event.dart
@@ -1,18 +1,14 @@
 part of 'bloc.dart';
 
-sealed class AuthEvent extends Equatable {
-  @override
-  List<Object?> get props => [];
-}
+sealed class AuthEvent {}
+
+sealed class RecoveryPasswordEvent {}
 
 final class LoginAccountEvent extends AuthEvent {
   final String username;
   final String password;
 
   LoginAccountEvent({required this.username, required this.password});
-
-  @override
-  List<Object?> get props => super.props..addAll([username, password]);
 }
 
 final class CreateAccountEvent extends AuthEvent {
@@ -20,14 +16,6 @@ final class CreateAccountEvent extends AuthEvent {
   final String password;
 
   CreateAccountEvent({required this.username, required this.password});
-
-  @override
-  List<Object?> get props => super.props..addAll([username, password]);
-}
-
-sealed class RecoveryPasswordEvent extends Equatable {
-  @override
-  List<Object?> get props => [];
 }
 
 final class CheckRecoveryCodeEvent extends RecoveryPasswordEvent {
@@ -35,9 +23,6 @@ final class CheckRecoveryCodeEvent extends RecoveryPasswordEvent {
   final String code;
 
   CheckRecoveryCodeEvent({required this.username, required this.code});
-
-  @override
-  List<Object?> get props => super.props..addAll([username, code]);
 }
 
 final class SaveNewPasswordEvent extends RecoveryPasswordEvent {
@@ -45,7 +30,4 @@ final class SaveNewPasswordEvent extends RecoveryPasswordEvent {
   final String newPassword;
 
   SaveNewPasswordEvent({required this.username, required this.newPassword});
-
-  @override
-  List<Object?> get props => super.props..addAll([username, newPassword]);
 }
--- lib/features/auth_screen/bloc/state.dart
+++ lib/features/auth_screen/bloc/state.dart
@@ -1,9 +1,8 @@
 part of 'bloc.dart';
 
-sealed class AuthState extends Equatable {
-  @override
-  List<Object?> get props => [];
-}
+sealed class AuthState {}
+
+sealed class RecoveryPasswordState {}
 
 final class AuthInitial extends AuthState {}
 
@@ -16,8 +15,6 @@ final class AuthFinished extends AuthState {
   final String? recoveryCode;
 
   AuthFinished({this.text, this.recoveryCode});
-  @override
-  List<Object?> get props => super.props..addAll([text, recoveryCode]);
 }
 
 final class AuthLogInError extends AuthState {}
@@ -28,14 +25,6 @@ final class AuthException extends AuthState {
   final Object? exception;
 
   AuthException({this.exception});
-
-  @override
-  List<Object?> get props => super.props..add(exception);
-}
-
-sealed class RecoveryPasswordState extends Equatable {
-  @override
-  List<Object?> get props => [];
 }
 
 final class RecoveryPasswordInitial extends RecoveryPasswordState {}

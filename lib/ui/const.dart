37423612f89adbe30bd4a9f541e234f0d5e5a620
encer <encer.e@yandex.ru>
2024-10-19 21:49:40 +0300

    Added manga status constants

--- lib/ui/const.dart
+++ lib/ui/const.dart
@@ -13,3 +13,36 @@ class SettingKeysConst {
   static const String sortedIndexKey = "sortedIndex";
   static const String sorterOrderKey = "sorterOrder";
 }
+
+class MangaStatusConst {
+  // All statuses must be in lowercase
+  static const List<String> releasedStatus = [
+    "закончен",
+    "released",
+    "done",
+  ];
+  static const List<String> ongoingStatus = [
+    "продолжается",
+    "лицензировано",
+    "ongoing",
+  ];
+  static const List<String> frozenStatus = [
+    "заморожен",
+  ];
+  static const List<String> otherStatus = [
+    "Нет переводчика",
+  ];
+
+  // Get int value from current manga status
+  // released = 1 -> ongoing = 2 -> frozen = 3 -> other status = 4
+  static int getProperty(String? mangaStatus) {
+    if (mangaStatus == null) return 2;
+    mangaStatus = mangaStatus.toLowerCase();
+    if (releasedStatus.contains(mangaStatus)) return 1;
+    if (ongoingStatus.contains(mangaStatus)) return 2;
+    if (frozenStatus.contains(mangaStatus)) return 3;
+    if (otherStatus.contains(mangaStatus)) return 4;
+
+    return 2;
+  }
+}

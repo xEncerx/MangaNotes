90f7b3115414d39610175d1ce2a075db07221733
encer <encer.e@yandex.ru>
2024-10-19 15:26:40 +0300

    Added swipe to dismiss for Snackbar

--- lib/features/widgets/snack_bar.dart
+++ lib/features/widgets/snack_bar.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 void showInfoSnackBar({
   required BuildContext context,
   required String text,
-  duration = const Duration(seconds: 3),
+  duration = const Duration(seconds: 2),
 }) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    duration: duration,
-    behavior: SnackBarBehavior.floating,
-    margin: const EdgeInsets.symmetric(horizontal: 10).copyWith(bottom: 10),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(10),
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      dismissDirection: DismissDirection.horizontal,
+      duration: duration,
+      behavior: SnackBarBehavior.floating,
+      margin: const EdgeInsets.symmetric(horizontal: 10).copyWith(bottom: 10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      content: Center(child: Text(text)),
     ),
-    content: Center(child: Text(text)),
-  ));
+  );
 }

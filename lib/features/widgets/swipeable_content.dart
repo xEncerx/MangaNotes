b62f9a94e87d96e12aa28079f51ea5a2dc81ccaa
encer <encer.e@yandex.ru>
2024-10-26 19:06:50 +0300

    Added widget to track screen swipe

---
+++ lib/features/widgets/swipeable_content.dart
@@ -0,0 +1,38 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+class SwipeableContent extends StatefulWidget {
+  final Widget child;
+  const SwipeableContent({super.key, required this.child});
+
+  @override
+  State<SwipeableContent> createState() => _SwipeableContentState();
+}
+
+class _SwipeableContentState extends State<SwipeableContent> {
+  double _dx = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: (details) => _onPanUpdate(details),
+      onPanEnd: (details) => _onPanEnd(details),
+      child: Transform.translate(
+        offset: Offset(_dx, 0),
+        child: widget.child,
+      ),
+    );
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    setState(() => _dx += details.delta.dx);
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    if (_dx > MediaQuery.of(context).size.width * 0.2) {
+      context.router.maybePop();
+    } else {
+      setState(() => _dx = 0);
+    }
+  }
+}
